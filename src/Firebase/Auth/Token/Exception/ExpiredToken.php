7006b3c6cc9d31ed77cba10d1d3476a2ebdb1a62
Luiz Henrique Gomes Palácio <lhpalacio@outlook.com>
2020-10-27 03:39:06 -0300

    Add missing space (#30)

--- src/Firebase/Auth/Token/Exception/ExpiredToken.php
+++ src/Firebase/Auth/Token/Exception/ExpiredToken.php
@@ -9,7 +9,7 @@ class ExpiredToken extends InvalidToken
     public function __construct(Token $token)
     {
         if ($expiredSince = \DateTimeImmutable::createFromFormat('U', $token->getClaim('exp'))) {
-            $message = "This token is expired since{$expiredSince->format(\DateTime::ATOM)}.";
+            $message = "This token is expired since {$expiredSince->format(\DateTime::ATOM)}.";
         } else {
             $message = 'This token is expired.';
         }
